58098ece242bc1ef1295e8abd6084a9f7f3d832d
jorgerisco <jariscog89@gmail.com>
2017-11-06 19:47:33 -0500

    Update Calculadora.java

--- src/main/java/com/example/Calculadora.java
+++ src/main/java/com/example/Calculadora.java
@@ -2,10 +2,8 @@ package com.example;
 
 public class Calculadora {
 
-    private int resultado;
-
     public int sumar (int num1, int num2) {
-        resultado = num1 + num2;
+        int resultado = num1 + num2;
 		return resultado;
     }
 
