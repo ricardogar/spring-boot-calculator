b8dfcf18049e6ce911cf58e964de91ce8617cb63
jorgerisco <jariscog89@gmail.com>
2017-11-06 19:47:02 -0500

    Update Calculadora.java

--- src/main/java/com/example/Calculadora.java
+++ src/main/java/com/example/Calculadora.java
@@ -9,27 +9,4 @@ public class Calculadora {
 		return resultado;
     }
 
-	public int restar (int num1, int num2) {
-        resultado = num1 - num2;
-		return resultado;
-    }
-	
-	public int multiplicar (int num1, int num2) {
-        resultado = num1 * num2;
-		return resultado;
-    }
-	
-	public int dividir (int num1, int num2) {
-        if(num2 == 0){
-			return num2;
-		}
-		resultado = num1 / num2;
-		return resultado;
-    }
-	/*
-	public int promediar (int num1, int num2) {
-		resultado = this.dividir(this.sumar(num1, num2), 2);
-		return resultado;
-    }
-	*/
 }
