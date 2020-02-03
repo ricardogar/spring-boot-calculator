fcfb04aa7bd6791edec3a4e34c1135d85101d5ff
ricardogar <librafisi@gmail.com>
2020-02-03 12:30:17 -0500

    Update IndexTest.java

--- src/test/java/com/example/IndexTest.java
+++ src/test/java/com/example/IndexTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,6 +25,11 @@ public class IndexTest {
     public void testSumaSinParametros() throws Exception {
 
         this.mockMvc.perform(get("/index"))
-			.andDo(print()).andExpect(status().isOk());
+            //.andDo(print()).andExpect(status().isOk());
+                .andDo(print()).andExpect(status().isOk());
     }
+
+    private ResultHandler print() {
+        return null;
+	}
 }
