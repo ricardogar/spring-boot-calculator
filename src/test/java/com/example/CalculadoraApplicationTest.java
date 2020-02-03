eecb2700d93293cc9d07b3c9f95d062515771f40
ricardogar <librafisi@gmail.com>
2020-02-03 01:03:41 -0500

    Update CalculadoraApplicationTest.java
    cc

--- src/test/java/com/example/CalculadoraApplicationTest.java
+++ src/test/java/com/example/CalculadoraApplicationTest.java
@@ -1,9 +1,10 @@
 package com.example;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
